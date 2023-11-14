dd54bfc4a5c885262f648f2af7526a54b71d9846
omazoz <oumaymamazoz12@gmail.com>
2023-11-14 14:58:14 +0100

    puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+ #we got this file
+exec {'sets file limite for nginx':
+  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /etc/default/nginx'
+}
