34c34584392a49614b04e00b542f38f817ded6f5
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2023-11-17 19:43:58 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,9 @@
 #we got this file
 exec {'sets file limite for nginx':
-  command => 'sed -i "s/15/4096/g" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
+}
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
